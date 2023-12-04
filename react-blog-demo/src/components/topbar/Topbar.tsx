c4a3d2065eca617daf156816d8627f7998e51243
Phornhathai <phornhathai.aum@gmail.com>
2023-12-04 21:41:38 +0700

    submit

--- react-blog-demo/src/components/topbar/Topbar.tsx
+++ react-blog-demo/src/components/topbar/Topbar.tsx
@@ -5,29 +5,55 @@ import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faSquarePinterest } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 export default function topbar() {
+  const user = false
   return (
     <div className="top">
       <div className="topLeft">
-      <FontAwesomeIcon icon={faSquareFacebook} className="topIcon" /> 
-      <FontAwesomeIcon icon={faSquareTwitter} className="topIcon" />
-      <FontAwesomeIcon icon={faSquarePinterest} className="topIcon" />
-      <FontAwesomeIcon icon={faSquareInstagram} className="topIcon" />
+        <FontAwesomeIcon icon={faSquareFacebook} className="topIcon" />
+        <FontAwesomeIcon icon={faSquareTwitter} className="topIcon" />
+        <FontAwesomeIcon icon={faSquarePinterest} className="topIcon" />
+        <FontAwesomeIcon icon={faSquareInstagram} className="topIcon" />
       </div>
       <div className="topCenter">
-      <ul className="topList">
-        <li className="topListItem">HOME</li>
-        <li className="topListItem">ABOUT</li>
-        <li className="topListItem">CONTACT</li>
-        <li className="topListItem">WRITE</li>
-        <li className="topListItem">LOGOUT</li>
-      </ul>
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">HOME</Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">ABOUT</Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">CONTACT</Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">WRITE</Link>
+          </li>
+          <li className="topListItem">
+            {user && "LOGOUT"}
+          </li>
+        </ul>
       </div>
       <div className="topRight">
-        <img className="topImg" src="https://images.pexels.com/photos/19225614/pexels-photo-19225614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="profile"  />
-      <FontAwesomeIcon icon={faSearch} className="topSearchIcon"/>
+        {
+          user ? (
+            <img className="topImg" src="https://images.pexels.com/photos/19225614/pexels-photo-19225614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="profile" />
+          ) : (
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">Login</Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/register">Register</Link>
+              </li>
+            </ul>
+          )
+        }
+
+        <FontAwesomeIcon icon={faSearch} className="topSearchIcon" />
       </div>
     </div>
   )
